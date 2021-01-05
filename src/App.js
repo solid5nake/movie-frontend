8c9c37c1669a440ade10817c02121539808b6092
Daniel <info@danielhospitality.nl>
2021-01-05 11:25:38 +0100

    TEST$
    test4

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
 
           hkjfshsfkjhjklshfklshflkshfslkhfslkfhslkfh
+
+          TEST3
+          Test3
         </p>
         <a
           className="App-link"
