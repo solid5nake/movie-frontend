5cb330c68e4fb8980ec34f7df25003f586284df3
Daniel <info@danielhospitality.nl>
2021-01-06 09:00:57 +0100

    Day1
    Day1 Prog

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>DAN IELBJbjkhjkhkjhk
-          Edit <code>src/App.js</code> and save to reload.
-
-          hkjfshsfkjhjklshfklshflkshfslkhfslkfhslkfh
-
-          TEST3
-          Test3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey BingeWatch Clone</h1>
+     
     </div>
   );
 }
